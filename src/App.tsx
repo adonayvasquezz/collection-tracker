35ae24ceadd7193dc74eb5238e0e0df54eba4604
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-15 16:39:07 -0600

    Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
 import Detail from './pages/Detail'
 import Home from './pages/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path='/collection/:id' element={<Detail />} />
+        <Route path='/collection' element={<Detail />} />
         
       </Routes>
     </BrowserRouter>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark" >
+        <div className="container ps-5 pe-5 d-flex justify-content-between">
+            <Link className="navbar-brand text-warning" to="/">
+                Collection Tracker
+            </Link>
+            <button className={`btn btn-warning ${styles.addButton}`} type="button">Agregar Colección</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
