3f9ebb22a045ce192470826a51579e7319996edc
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-16 02:08:07 -0600

    Form completed with basic validations

--- src/components/FormCollection/FormCollection.tsx
+++ src/components/FormCollection/FormCollection.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Collection } from '../../interfaces/collection';
 import { apiPost } from '../../resources/apiResources';
+import { currentDate } from '../../utilities';
 
 interface Props {
     closeModal: () => void
@@ -10,25 +11,24 @@ const FormCollection:FC<Props> = ({ closeModal=()=>{} }) => {
 
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-
-    const handleName = (e: { target: { value: string }; }) => {
-        setName(e.target.value);
-    }
-    const handleDescription = (e: { target: { value: string }; }) => {
-        setDescription(e.target.value);
-    }
+    const [category, setCategory] = useState('');
+    const [value, setValue] = useState(0);
+    const [date, setDate] = useState('');
+    const [condition, setCondition] = useState('');
+    const [location, setLocation] = useState('');
+    const [image, setImage] = useState('');
 
     const handleSubmitForm = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         const data:Collection = {
             name: name,
             description: description,
-            category:    'string',
-            value:       5,
-            year:        Date.now().toString(),
-            condition:   'string',
-            location:    'string',
-            image:       'string',
+            category:    category,
+            value:       value,
+            year:        date,
+            condition:   condition,
+            location:    location,
+            image:       image,
         };
 
         formRequest(data, '')
@@ -52,12 +52,45 @@ const FormCollection:FC<Props> = ({ closeModal=()=>{} }) => {
             <div className="col-12 col-md-6 col-lg-6 mt-5">
                 <form onSubmit={handleSubmitForm}>
                     <div className="mb-3">
-                        <label className="form-label">Collection Name</label>
-                        <input type="text" className="form-control" required id="name" onChange={handleName} value={name}></input>
+                        <label >Collection Name</label>
+                        <input type="text" className="form-control" required id="name" 
+                        onChange={ e=>setName(e.target.value) } value={name}></input>
                     </div>
                     <div className="mb-3">
-                        <label className="form-label">Description</label>
-                        <textarea className="form-control" id="description" onChange={handleDescription} value={description} ></textarea>
+                        <label >Description</label>
+                        <textarea className="form-control" id="description" 
+                        onChange={ e=>setDescription(e.target.value) } value={description} ></textarea>
+                    </div>
+                    <select className="form-select" required id="category" onChange={ e=>setCategory(e.target.value) } aria-label="collection category">
+                        <option value="Music">Music</option>
+                        <option value="Art" >Art</option>
+                        <option value="Coin">Coins</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <div className="mb-3">
+                        <label >Value</label>
+                        <input type="number" className="form-control" id="value" min="0" max="100000"
+                        onChange={ e=>setValue(parseInt(e.target.value)) } defaultValue={0}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label >Year</label>
+                        <input type="date" className="form-control" id="date" max={currentDate()} /* Max date validation */
+                        onChange={ e=>setDate(e.target.value) } defaultValue={date}></input>
+                    </div>
+                    <select className="form-select" onChange={ e=>setCondition(e.target.value) } aria-label="collection condition">
+                        <option value="New">New</option>
+                        <option value="Good" >Good</option>
+                        <option value="Damaged">Damaged</option>
+                    </select>
+                    <div className="mb-3">
+                        <label >Location</label>
+                        <input type="text" className="form-control" id="location" 
+                        onChange={ e=>setLocation(e.target.value) } value={location}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label >Online Image</label>
+                        <input type="text" className="form-control" id="image" 
+                        onChange={ e=>setImage(e.target.value) } value={image}></input>
                     </div>
                     
                     <div className="d-flex justify-content-center">
--- src/utilities.tsx
+++ src/utilities.tsx
@@ -2,10 +2,25 @@
 // Add Modal styles
 export const modalStyles = {
     content: {
-      top: '30%',
+      top: '50%',
       left: '50%',
       right: '5%',
       bottom: 'auto',
       transform: 'translate(-50%, -50%)',
     },
   };
+
+  export const currentDate = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //Months start with 0
+    let yyyy = today.getFullYear();
+    let d:string ='', m:string ='', actual:string = '';
+
+    d = dd<10 ? `0${dd}` : `${dd}`;
+    m = mm<10 ? `0${mm}` : `${mm}`;
+ 
+    actual = `${yyyy}-${m}-${d}`;
+
+   return actual;
+  }
