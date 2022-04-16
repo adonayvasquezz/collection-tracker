06d6e90124604dd27c3524a3040937c053d62534
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-16 03:09:44 -0600

    Icons for edit and delete collections

--- src/components/CollectionItem/CollectionItem.tsx
+++ src/components/CollectionItem/CollectionItem.tsx
@@ -1,4 +1,5 @@
 import {FC} from 'react';
+import { Link } from 'react-router-dom';
 import { Collection } from '../../interfaces/collection';
 import styles from './CollectionItem.module.css';
 
@@ -13,8 +14,17 @@ const CollectionItem: FC<Props> = ({collection}, {id}) => {
       <div className="card-body">
         <h5 className="card-title">{collection.name}</h5>
         <p className="card-text">{collection.description}</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        
       </div>
+      <div className='d-flex justify-content-between mb-2'>
+        <Link to="/collection" className="btn btn-secondary">Watch Details</Link>
+        <span>
+          <i className={`fas fa-edit mx-2 link-success ${styles.isClickeable}`} onClick={() => console.log('edited')} />
+          <i className={`fas fa-trash-alt mx-2 link-danger ${styles.isClickeable}`} onClick={() => console.log('deleted')}  />
+        </span>
+      </div>
+      
+      
     </div>
   )
 }
