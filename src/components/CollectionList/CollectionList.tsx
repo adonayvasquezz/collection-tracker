5d0e571ae81d8a70e7c1b2bfd5dfde8c1ee827a8
Adonay Vasquez <adonay16.vz@gmail.com>
2022-04-17 03:09:12 -0600

    Validation ask for delete a collection

--- src/components/CollectionList/CollectionList.tsx
+++ src/components/CollectionList/CollectionList.tsx
@@ -13,11 +13,14 @@ ReactModal.setAppElement('#root');
 const CollectionList = () => {
 
     const [modalOpen, setModalOpen] = useState(false);
+    const [modalDelete, setModalDelete] = useState(false);
+    const [idToDelete, setIdToDelete] = useState('');
+
     let navigate = useNavigate();
-    const collectionsGlobal = useSelector((state:{collections:Collection[]}) => state.collections);
+    const collectionsGlobal = useSelector((state: { collections: Collection[] }) => state.collections);
 
     useEffect(() => {
-        
+
         setCollections();
     }, [])
 
@@ -25,39 +28,44 @@ const CollectionList = () => {
     const setCollections = () => {
         apiGetAll('GET', 'tracker')
             .then(resp => {
-                store.dispatch({type: 'GET_ALL', collections: resp});
+                store.dispatch({ type: 'GET_ALL', collections: resp });
             })
             .catch(console.error);
     }
 
     // Render collections from global state
     const renderCollections = () => {
-        if (collectionsGlobal.length>0) {
+        if (collectionsGlobal.length > 0) {
             let collectionItems = collectionsGlobal.map((collection: Collection, id: number) => {
                 return <CollectionItem collection={collection} key={id} callEditCollection={callEditCollection} callDeleteCollection={callDeleteCollection} />
             });
             return collectionItems;
-        } 
+        }
     }
 
-    const callDeleteCollection = (idC:string):void => {
-        let deleteCollection = collectionsGlobal.find(e => e._id===idC);
-        apiDeleteCollection(idC)
+    const callDeleteCollection = (idC: string): void => {
+        setIdToDelete(idC);
+        setModalDelete(true);
+    }
+
+    const deleteCollection = () => {
+        let deleteCollection = collectionsGlobal.find(e => e._id === idToDelete);
+        apiDeleteCollection(idToDelete)
             .then(res => {
-                store.dispatch({type: 'DELETE', collection: deleteCollection});
+                store.dispatch({ type: 'DELETE', collection: deleteCollection });
             })
             .catch(console.error);
-        
-        //setCollections();
+
+        closeModal();
     }
 
-    const callEditCollection = (collect:Collection):void => {
-        navigate("/", {state: collect})
+    const callEditCollection = (collect: Collection): void => {
+        navigate("/", { state: collect })
         openModal();
     }
 
     const addCollection = () => {
-        navigate("/", {state: {}})
+        navigate("/", { state: {} })
         openModal();
     }
 
@@ -66,21 +74,22 @@ const CollectionList = () => {
     }
     const closeModal = () => {
         setModalOpen(false);
+        setModalDelete(false);
     }
 
     return (
         <div className={styles.containerListCards}>
 
             <div className={styles.buttonContainer}>
-                <button className={`btn btn-warning ${styles.addButton}`} 
-                        type="button"
-                        onClick={addCollection}>
+                <button className={`btn btn-warning ${styles.addButton}`}
+                    type="button"
+                    onClick={addCollection}>
                     Agregar Colección
                 </button>
             </div>
 
             <div className="row">
-                { renderCollections() ? renderCollections() : 'No data'}
+                {renderCollections() ? renderCollections() : 'No data'}
             </div>
 
             <ReactModal
@@ -91,6 +100,23 @@ const CollectionList = () => {
             >
                 <FormCollection closeModal={closeModal} setCollections={setCollections} />
             </ReactModal>
+            <ReactModal
+                isOpen={modalDelete}
+                onRequestClose={closeModal}
+                className={styles.modalDelete}
+                contentLabel="Confirmation delete"
+            >
+                <div className="p-5 d-flex justify-content-center">
+                    <div>
+                        <h4 className="text-center">Are you sure?</h4>
+                        <button className="btn btn-secondary mx-1" onClick={closeModal}>Cancel</button>
+                        <button className="btn btn-danger mx-1" onClick={deleteCollection}>Yes, delete</button>
+                    </div>
+                </div>
+
+
+            </ReactModal>
+
         </div>
     )
 }
